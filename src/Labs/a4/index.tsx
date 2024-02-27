a830c929b03e3744d5b13c88a903ab7cd50d7a11
maggieliu <magliudh@gmail.com>
2024-02-27 15:49:41 -0500

    finished lab before exam material

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,9 +1,36 @@
-function Assignment4() {
-    return (
-      <div className="container">
-        <h1>Assignment 3</h1>
-      </div>
-    );
-   }
-   
-     export default Assignment4;
+import React from "react";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+
+const Assignment4 = () => {
+  function sayHello() {
+    alert("Hello");
+  }
+
+  return(
+    <>
+      <h1>Assignment 4</h1>
+      <ClickEvent/>
+      <PassingDataOnEvent/>
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject/>
+      <Counter/>
+      <BooleanStateVariables/>
+      <StringStateVariables/>
+      <DateStateVariable/>
+      <ObjectStateVariable/>
+      <ArrayStateVariable/>
+      <ParentStateComponent/>
+    </>
+  );
+};
+export default Assignment4;
